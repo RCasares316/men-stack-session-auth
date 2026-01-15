629f72d5454a65f229b6d59a7b370113fbf8e89a
RCasares316 <richiecasares@gmail.com>
2026-01-15 14:11:17 -0500

    auth lesson completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,65 @@
+const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
+
 const register = (req, res) => {
   res.render("auth/register.ejs");
 };
 
+const registerUser = async (req, res) => {
+  //ensure username isnt taken
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (userInDatabase) {
+    retures.send("Username already taken.");
+  }
+  //ensure password and confirm password match
+  if (req.body.password !== req.body.confirmPassword) {
+    return res.send("Password and Confirm Password must match");
+  }
+  //hash password
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+
+  //creat user
+  await User.create({ username: req.body.username, password: hashedPassword });
+
+  //redirect
+  res.redirect("/");
+};
+
+const login = (req, res) => {
+  res.render("auth/login.ejs");
+};
+
+const loginUser = async (req, res) => {
+  //ensure username is correct
+  const userInDatabase = await User.findOne({ username: req.body.username });
+  if (!userInDatabase) {
+    return res.send("Login failed. Please try again.");
+  }
+  //ensure password is correct
+  const validPassword = bcrypt.compareSync(
+    req.body.password,
+    userInDatabase.password
+  );
+  if (!validPassword) {
+    return res.send("Login failed. Please try again.");
+  }
+  //mange
+  req.session.user = {
+    username: userInDatabase.username,
+    _id: userInDatabase._id,
+  };
+  res.redirect("/");
+};
+
+const signOut = (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+};
+
 module.exports = {
   register,
+  registerUser,
+  login,
+  loginUser,
+  signOut,
 };
--- routes/auth.js
+++ routes/auth.js
@@ -3,5 +3,9 @@ const controllers = require("../controllers/auth");
 const router = Router();
 
 router.get("/register", controllers.register)
+router.post("/register", controllers.registerUser)
+router.get("/login", controllers.login)
+router.post("/login", controllers.loginUser)
+router.get("/sign-out", controllers.signOut)
 
 module.exports = router
